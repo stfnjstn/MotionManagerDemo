2e732eedae02474f6939f3d1a8245a2edd65b67b
stfnjstn <stfn.jstn@gmail.com>
2015-04-17 23:49:41 +0200

    Restore deletet files

---
+++ MotionManagerDemo/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  MotionManagerDemo
+//
+
+//  Copyright (c) 2014 MotionManagerDemo. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
